ecabe574c047da1233904fba304ed3390b0b03b0
unknown <Thathalas@Thathalas-PC.(none)>
2012-11-18 23:13:44 +0100

    Added comments for the Receiver

--- src/basic/chat/HuebnerReceiver.java
+++ src/basic/chat/HuebnerReceiver.java
@@ -3,13 +3,28 @@ package basic.chat;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 
+/**
+ * Emfanger für den HuebnerChat
+ * 
+ * Wird verwendet um Packete von anderen Clienten zu empfangen
+ *
+ */
 final class HuebnerReceiver implements Runnable {
 	private final HuebnerChat huebnerChat;
 
+	/**
+	 * Standardkonstrucktor
+	 * Setzt den Chat(die Verbindung)
+	 * 
+	 * @param huebnerChat Der aktuelle Chat des Clienten
+	 */
 	HuebnerReceiver(HuebnerChat huebnerChat) {
 		this.huebnerChat = huebnerChat;
 	}
 
+	/**
+	 * Horcht auf dem Port 50001 nach Paketen von anderen Clienten
+	 */
 	public void run() {
 		try {
 			DatagramSocket clientSocket = new DatagramSocket(50001);
@@ -18,9 +33,9 @@ final class HuebnerReceiver implements Runnable {
 			while (true) {
 				clientSocket.receive(receivePacket);
 				String message = new String(receivePacket.getData(), 0, receivePacket.getLength());
-				String[] split = message.split(": ", 2);
+				String[] split = message.split(": ", 2);	//Spalte die nachricht in [0] Benutzer und [1] Nachricht
 				this.huebnerChat.sysout.println(split[0].replace('_', ' ') + ": " + split[1]);
-				this.huebnerChat.getGUI().appendText(split[0].replace('_', ' ') + ": " + split[1], HuebnerGUI.SET_ORANGE);
+				this.huebnerChat.getGUI().appendText(split[0].replace('_', ' ') + ": " + split[1], HuebnerGUI.SET_ORANGE); //Gebe die Packete auf der GUI aus
 			}
 			//clientSocket.close();
 		} catch (Exception e) {
